9b167caf512b68c7849bec48dba314e4beba15fe
Saumya6785 <saumyatiwari530@gmail.com>
2026-01-06 21:19:04 +0530

    Update cte.sql

--- 03_advanced_sql/cte.sql
+++ 03_advanced_sql/cte.sql
@@ -1,1 +1,12 @@
+WITH customer_revenue AS (
+    SELECT
+        o.customer_id,
+        SUM(s.total_amount) AS revenue
+    FROM orders o
+    JOIN sales s ON o.order_id = s.order_id
+    GROUP BY o.customer_id
+)
+SELECT *
+FROM customer_revenue
+ORDER BY revenue DESC;
 
