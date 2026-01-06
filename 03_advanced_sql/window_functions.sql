ff8f3247a659b15c8562f939026569b4b068ac69
Saumya6785 <saumyatiwari530@gmail.com>
2026-01-06 21:19:31 +0530

    Update window_functions.sql

--- 03_advanced_sql/window_functions.sql
+++ 03_advanced_sql/window_functions.sql
@@ -1,1 +1,8 @@
+SELECT
+    customer_id,
+    SUM(total_amount) AS total_revenue,
+    RANK() OVER (ORDER BY SUM(total_amount) DESC) AS rank_position
+FROM orders o
+JOIN sales s ON o.order_id = s.order_id
+GROUP BY customer_id;
 
